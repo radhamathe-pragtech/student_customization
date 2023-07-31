4e8be0aa90f61bd602f880554983e016183f478c
radhamathe-pragtech <radha.mathe@pragtech.co.in>
2023-07-31 13:01:03 +0530

    [ADD] file

--- student/__manifest__.py
+++ student/__manifest__.py
@@ -25,8 +25,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/view_student.xml',
-        # 'views/views.xml',
+        # 'views/view_student.xml',
+        'views/views.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- student/models/models.py
+++ student/models/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+
+from odoo import fields, models, api
+
+class Student(models.Model):
+    _name = "student.student"
+    _decsription = "Student"
+
+    name = fields.Char(string="Student Name")
+    age = fields.Integer(string="Age")
+    classes = fields.Char(string="Class Name")
+
 
 
 # class student(models.Model):
